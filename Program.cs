d14962dd4c4bf35d148966760a183670d5f74a99
Jhônata Tavares <137582138+Jhonatayu@users.noreply.github.com>
2025-01-08 07:23:17 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Gerenciamento_de_Produtos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
