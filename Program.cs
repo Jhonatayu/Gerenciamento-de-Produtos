a04feee9c629f47a01a42224d21a29d43e65df4e
Jhônata Tavares <137582138+Jhonatayu@users.noreply.github.com>
2025-01-08 07:28:19 -0300

    Libs add

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Gerenciamento_de_Produtos
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerenciamento_de_Produtos
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
         }
     }
 }
