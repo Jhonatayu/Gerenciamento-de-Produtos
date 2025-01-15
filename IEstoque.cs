41b4ecd106e561f71eb6c5c33657a647309433cc
Jhônata Tavares <137582138+Jhonatayu@users.noreply.github.com>
2025-01-15 06:56:35 -0300

    Save Implemented

--- IEstoque.cs
+++ IEstoque.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
 
 namespace Gerenciamento_de_Produtos
 {
--- Produto.cs
+++ Produto.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
 
 namespace Gerenciamento_de_Produtos
 {
+    [System.Serializable]
     abstract class Produto
     {
         public string Id { get; set; }
